d1ae9612078c913879d3118e2c192c1abfafdc17
WarLock54 <onr_540@hotmail.com>
2026-03-11 15:13:41 +0300

    fix: remove dj_database_url import

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-import dj_database_url  # Başına bunu ekleyin
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,7 +109,6 @@ DATABASES = {
 }
 # DATABASES bloğunu tamamen bununla değiştirin:
 #DATABASES = {
- #   'default': dj_database_url.config(
  #       default="postgres://admin:admin@50.85.248.188:5432/djangoapp",
  #       conn_max_age=600
  #   )
